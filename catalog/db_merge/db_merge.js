0e888dba3a92b5f31fea33729b19bb7c622071a0
uchida <linux@hi-ho.ne.jp>
2016-12-26 15:00:15 +0900

    Commit on Dec/26/2016 PM 15:00

--- catalog/db_merge/db_merge.js
+++ catalog/db_merge/db_merge.js
@@ -2,53 +2,60 @@
 // ---------------------------------------------------------------
 //	db_merge.js
 //
-//					Mar/22/2014
+//					Dec/26/2016
 //
 // ---------------------------------------------------------------
-var fs = require("fs");
+var fs = require("fs")
 
 // ---------------------------------------------------------------
 function file_parse_proc (fname_in)
 {
-	var json_str = fs.readFileSync (fname_in,'utf8');
-	var db_out = JSON.parse (json_str);
+	const json_str = fs.readFileSync (fname_in,'utf8')
+	const db_out = JSON.parse (json_str)
 
-	return	db_out;
+	return	db_out
 }
 
 // ---------------------------------------------------------------
-console.log ("*** 開始 ***");
+console.log ("*** 開始 ***")
 //
-var json_books=process.argv[2];
-var json_authors=process.argv[3];
-var json_readers=process.argv[4];
-var json_genres=process.argv[5];
-var json_languages=process.argv[6];
-var json_out=process.argv[7];
+const json_books=process.argv[2]
+const json_authors=process.argv[3]
+const json_readers=process.argv[4]
+const json_genres=process.argv[5]
+const json_languages=process.argv[6]
+const json_out=process.argv[7]
 
-console.log (json_books);
-console.log (json_authors);
-console.log (json_readers);
-console.log (json_genres);
-console.log (json_languages);
-console.log (json_out);
+console.log (json_books)
+console.log (json_authors)
+console.log (json_readers)
+console.log (json_genres)
+console.log (json_languages)
+console.log (json_out)
 
-var db_books = file_parse_proc (json_books);
-var db_authors = file_parse_proc (json_authors);
-var db_readers = file_parse_proc (json_readers);
-var db_genres = file_parse_proc (json_genres);
-var db_languages = file_parse_proc (json_languages);
+/*
+var db_books = file_parse_proc (json_books)
+var db_authors = file_parse_proc (json_authors)
+var db_readers = file_parse_proc (json_readers)
+var db_genres = file_parse_proc (json_genres)
+var db_languages = file_parse_proc (json_languages)
+*/
 
-var db_out = new Object ();
-db_out['books']=db_books;
-db_out['authors']=db_authors;
-db_out['readers']=db_readers;
-db_out['genres']=db_genres;
-db_out['languages']=db_languages;
+var db_out = new Object ()
+db_out['books']= file_parse_proc (json_books)
+db_out['authors']= file_parse_proc (json_authors)
+db_out['readers']= file_parse_proc (json_readers)
+db_out['genres']= file_parse_proc (json_genres)
+db_out['languages']= file_parse_proc (json_languages)
 
-var json_str_out = JSON.stringify (db_out);
+const json_str_out = JSON.stringify (db_out)
 
-fs.writeFile (json_out,json_str_out);
+fs.writeFile (json_out,json_str_out,function (err)
+	{
+	f (err) {
+		console.error ("Error on write: " + err)
+		}
+	})
 
-console.log ("*** 終了 ***");
+console.log ("*** 終了 ***")
 // ---------------------------------------------------------------
